7e811cc88b58b3e5db94c02fb2ec1f9aad783320
Roman Permiakov <info@presstomsk.ru>
2022-08-22 19:48:30 +0700

    Добавление DbSet

--- AppForWorkingWithXML/DB/MainContext.cs
+++ AppForWorkingWithXML/DB/MainContext.cs
@@ -8,8 +8,29 @@ namespace AppForWorkingWithXML.DB
     { 
                 
         public DbSet<AddressObjectsObject> AddressObjects { get; set; }
+        public DbSet<AsAddrObjDivisionItemsItem> AsAddrObjDivisionItems { get; set; }
+        public DbSet<AddressObjectTypesAddressObjectType> AddressObjectTypes { get; set; }
+        public DbSet<AsAdmHierarchyItemsItem> AsAdmHierarchyItems { get; set; }
+        public DbSet<ApartmentsApartment> Apartments { get; set; }
+        public DbSet<ApartmentTypesApartmentType> ApartmentTypes { get; set; }
+        public DbSet<CarplacesCarplace> Carplaces { get; set; }
+        public DbSet<AsChangeHistoryItemsItem> AsChangeHistoryItems { get; set; }
+        public DbSet<HousesHouse> Houses { get; set; }
+        public DbSet<HouseTypesHouseType> HouseTypes { get; set; }
+        public DbSet<AsMunHierarchyItemsItem> AsMunHierarchyItems { get; set; }
+        public DbSet<NormativeDocsNormativeDoc> NormativeDocs { get; set; }
+        public DbSet<NormativeDocsKind> NormativeDocsKinds { get; set; }
+        public DbSet<NormativeDocsType> NormativeDocsTypes { get; set; }
+        public DbSet<ObjectLevelsObjectLevel> ObjectLevels { get; set; }
+        public DbSet<OperationTypesOperationType> OperationTypes { get; set; }
+        public DbSet<ParamsParam> Params { get; set; }
+        public DbSet<ParamTypesParamType> ParamTypes { get; set; }
+        public DbSet<ReestrObjectsObject> ReestrObjects { get; set; }
+        public DbSet<RoomsRoom> Rooms { get; set; }
+        public DbSet<RoomTypesRoomType> RoomTypes { get; set; }
+        public DbSet<SteadsStead> Steads { get; set; }
 
-        
+
         public MainContext(){}
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
